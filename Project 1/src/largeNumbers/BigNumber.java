66676f1d7d2c15681e143c0f511de7e85eab345c
Eugene Fedotov <fedoto91@students.rowan.edu>
2014-09-16 20:39:17 -0400

    init

---
+++ Project 1/src/largeNumbers/BigNumber.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package largeNumbers;
+
+/**
+ * @author Eugene Fedotov
+ *
+ */
+public class BigNumber {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
