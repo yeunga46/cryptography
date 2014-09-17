d485244e07ebd6ba2752af039bb4381f381c193d
Eugene Fedotov <fedoto91@students.rowan.edu>
2014-09-16 22:28:11 -0400

    added: constructor and driver

--- Project 1/src/largeNumbers/BigNumber.java
+++ Project 1/src/largeNumbers/BigNumber.java
@@ -3,18 +3,25 @@
  */
 package largeNumbers;
 
+import java.util.ArrayList;
+
 /**
  * @author Eugene Fedotov
  *
  */
 public class BigNumber {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	char[] charArray = {};
+	ArrayList<Integer> number = new ArrayList<>();	
+	
+	BigNumber(String val){
+		charArray = val.toCharArray();
+		for (char ch: charArray) {
+			int a = Character.getNumericValue(ch);
+			number.add(a);
+		}
 	}
-
+	
+	ArrayList<Integer> getNumber(){
+		return number;
+	}
 }
---
+++ Project 1/src/largeNumbers/driver.java
@@ -0,0 +1,10 @@
+package largeNumbers;
+
+public class driver {
+
+	public static void main(String[] args) {
+		BigNumber number = new BigNumber("1234");
+		System.out.print("Number is " + number.getNumber());
+	}
+
+}
